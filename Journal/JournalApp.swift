7ce88b6f59e7eb779eb1d63175bbdcb2284e75ca
Yukiko Nii <NIIY64308@gmail.com>
2024-07-23 13:31:49 +0900

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Yukiko Nii on 2024/07/23.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
